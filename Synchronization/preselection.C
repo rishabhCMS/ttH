2109a419cc3113559ccc4a7abc61c7a865304264
Andres Vargas <vargasa@cua.edu>
2019-06-20 10:21:33 +0200

    Set uniform maxPt

--- Synchronization/preselection.C
+++ Synchronization/preselection.C
@@ -2,7 +2,9 @@
 
   TFile *f = TFile::Open("testNanoAOD_NANO.root");
   auto t1 = (TTree*)f->Get("Events");
-  auto c1 = new TCanvas("c1","c1");
+  auto c1 = new TCanvas("c1","c1", 500, 500);
+
+  float maxPt = 300.;
 
   // Preselection for Muon and Electrons from:
   // https://gitlab.cern.ch/ttH_leptons/doc/blob/master/Legacy/objects.md#2151-preselection
@@ -10,6 +12,8 @@
   // Electron
 
   t1->Draw("Electron_pt");
+  TH1F *hEle = (TH1F*)gPad->GetPrimitive("htemp");
+  hEle->GetXaxis()->SetRangeUser(0., maxPt);
   c1->Print("Electron_pt_NoCuts.png");
 
   TCut ePt = "Electron_pt>7";
@@ -23,16 +27,17 @@
   Int_t count = t1->Draw("Electron_pt",
                          ePt && eEta && eDxy && eDz
                          && eMIso && eSip3d && eWPLoose);
+  hEle = (TH1F*)gPad->GetPrimitive("htemp");
+  hEle->GetXaxis()->SetRangeUser(0., maxPt);
   cout << "Electron preselection events: " << count << endl;
-  TH1F *hEle = (TH1F*)gPad->GetPrimitive("htemp");
-  hEle->GetXaxis()->SetRangeUser(0.,300.);
   hEle->SetTitle("Electron Pt after Preselection;Pt;Counts");
-
   c1->Print("Electron_pt_PreselectionCuts.png");
 
   // Muon
 
   t1->Draw("Muon_pt");
+  TH1F *hMu = (TH1F*)gPad->GetPrimitive("htemp");
+  hMu->GetXaxis()->SetRangeUser(0., maxPt);
   c1->Print("Muon_pt_NoCuts.png");
 
   TCut muPt = "Muon_pt>5";
@@ -46,9 +51,10 @@
   count = t1->Draw("Muon_pt",
                    muPt && muEta && muDxy && muDz
                    && muMIso && muSip3d && nMuon);
+  hMu = (TH1F*)gPad->GetPrimitive("htemp");
+  hMu->GetXaxis()->SetRangeUser(0., maxPt);
   cout << "Muon preselection events: " << count << endl;
-  TH1F *hMu = (TH1F*)gPad->GetPrimitive("htemp");
-  hMu->GetXaxis()->SetRangeUser(0.,300.);
+
   hMu->SetTitle("Muon Pt after Preselection;Pt;Counts");
 
   c1->Print("Muon_pt_PreselectionCuts.png");
