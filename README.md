bbc8f78c13246811fef357c4c9a11fbbc60fb2cb
Andres Vargas <vargasa@cua.edu>
2019-06-20 09:58:51 +0200

    Add NoCuts plots

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ttH
+
+
+## Documentation
+
+* http://cms.cern.ch/iCMS/analysisadmin/cadilines?line=HIG-18-019&tp=an&id=2099&ancode=HIG-18-019
--- Synchronization/preselection.C
+++ Synchronization/preselection.C
@@ -7,6 +7,11 @@
   // Preselection for Muon and Electrons from:
   // https://gitlab.cern.ch/ttH_leptons/doc/blob/master/Legacy/objects.md#2151-preselection
 
+  // Electron
+
+  t1->Draw("Electron_pt");
+  c1->Print("Electron_pt_NoCuts.png");
+
   TCut ePt = "Electron_pt>7";
   TCut eEta = "abs(Electron_eta)<2.5";
   TCut eDxy = "Electron_dxy<=0.05";
@@ -23,7 +28,12 @@
   hEle->GetXaxis()->SetRangeUser(0.,300.);
   hEle->SetTitle("Electron Pt after Preselection;Pt;Counts");
 
-  c1->Print("electron.png");
+  c1->Print("Electron_pt_PreselectionCuts.png");
+
+  // Muon
+
+  t1->Draw("Muon_pt");
+  c1->Print("Muon_pt_NoCuts.png");
 
   TCut muPt = "Muon_pt>5";
   TCut muEta = "abs(Muon_eta)<2.4";
@@ -41,6 +51,17 @@
   hMu->GetXaxis()->SetRangeUser(0.,300.);
   hMu->SetTitle("Muon Pt after Preselection;Pt;Counts");
 
-  c1->Print("muon.png");
+  c1->Print("Muon_pt_PreselectionCuts.png");
 
+  // Jets
+
+  t1->Draw("Jet_pt");
+  c1->Print("Jet_pt_NoCuts.png");
+
+  TCut jetPt = "Jet_pt>25";
+  TCut jetEta = "abs(Jet_eta)<2.4";
+
+  count = t1->Draw("Jet_pt",
+                   jetPt && jetEta);
+  cout << "Jet preselection events: " <<  count << endl;
 }
